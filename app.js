f3f2c2bcad2b86acbad024f4f0151dbc176fe042
Alejandro Bernal <alejandro.bernal@urbantxt.com>
2023-06-28 23:16:52 -0700

    Refactored code structure

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-app.use(express.static(__dirname+"public" ))
 const PORT = 3000;
 
+app.use(express.static(__dirname + "/public" ));
+
 app.use('/home', (req,res) => {
-fs.readFile('../frontend/home.html',(err,data)=>{
+fs.readFile('./home.html',(err,data)=>{
     res.writeHead(200,{'Content-type': 'text/html'});
     res.write(data);
     return res.end();
@@ -14,7 +15,7 @@ fs.readFile('../frontend/home.html',(err,data)=>{
 });
 
 app.use('/about', (req,res) => {
-    fs.readFile('../frontend/about.html',(err,data)=>{
+    fs.readFile('./pages/about.html',(err,data)=>{
         res.writeHead(200,{'Content-type': 'text/html'});
         res.write(data);
         return res.end();
@@ -22,14 +23,14 @@ app.use('/about', (req,res) => {
     });
 
     app.use('/contact', (req,res) => {
-        fs.readFile('../frontend/contact.html',(err,data)=>{
+        fs.readFile('./pages/contact.html',(err,data)=>{
             res.writeHead(200,{'Content-type': 'text/html'});
             res.write(data);
             return res.end();
         })
         });
 app.use('/error', (req,res) => {
-        fs.readFile('../frontend/error.html',(err,data)=>{
+        fs.readFile('./pages/error.html',(err,data)=>{
             res.writeHead(200,{'Content-type': 'text/html'});
             res.write(data);
             return res.end("401 Error");
